069c789a52436d5e426edcdc8fca4660df7f0a5f
peterchoi <peterchoi0618@gmail.com>
2023-04-13 13:38:42 +0900

    [볼링] sprae 점수 계산(중간 프레임)

--- src/bowling/bowling-game.ts
+++ src/bowling/bowling-game.ts
@@ -1,11 +1,27 @@
 export class BowlingGame {
-  private totalScore = 0;
+  // 보너스 점수를 계산하려면 roll 자체를 저장하는 것이 필요해보임.
+  private rolls: number[] = [];
 
-  roll(number: number) {
-    this.totalScore += number;
+  roll(pin: number) {
+    this.rolls.push(pin);
   }
 
   score() {
-    return this.totalScore;
+    let totalScore = 0;
+
+    // 기본 점수 합산
+    for (let i = 0; i < this.rolls.length; i++) {
+      totalScore += this.rolls[i];
+    }
+
+    // spare 보너스 점수 계산
+    for (let j = 0; j < this.rolls.length; j += 2) {
+      if (this.rolls[j] + this.rolls[j + 1] === 10) {
+        // spare 이면
+        totalScore += this.rolls[j + 2];
+      }
+    }
+
+    return totalScore;
   }
 }
--- src/bowling/bowling.test.ts
+++ src/bowling/bowling.test.ts
@@ -21,4 +21,14 @@ describe('bowling test', () => {
     }
     expect(g.score()).toEqual(0);
   });
+
+  it('중간 프레임에 spare 가 있는 경우', () => {
+    g.roll(3);
+    g.roll(7); // spare
+    g.roll(5);
+    for (let i = 0; i < 17; i++) {
+      g.roll(0);
+    }
+    expect(g.score()).toEqual(20);
+  });
 });
